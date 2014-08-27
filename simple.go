eb7d4aa6997ce67f7dffe05df6a73e959ee96f17
Michael Stahnke <stahnma@puppetlabs.com>
2014-08-26 17:27:54 -0700

    WIP

--- simple.go
+++ simple.go
@@ -1,39 +1,61 @@
 package main
 
 import (
-    "github.com/danryan/hal"
-    _ "github.com/danryan/hal/adapter/shell"
-    _ "github.com/danryan/hal/store/memory"
-    "os"
+	"github.com/danryan/hal"
+	_ "github.com/danryan/hal/adapter/hipchat"
+	_ "github.com/danryan/hal/store/memory"
+	"os"
+	"stahnma.com/stahnma/handler"
 )
 
-var pingHandler = hal.Hear(`ping`, func(res *hal.Response) error {
-    return res.Send("PONG")
-})
+//var echoHandler = hal.Respond(`echo (.+)`, func(res *hal.Response) error {
+//  return res.Reply(res.Match[1])
+//})
 
-var echoHandler = hal.Respond(`echo (.+)`, func(res *hal.Response) error {
-    return res.Reply(res.Match[1])
+var fooHandler = hal.Respond(`foo`, func(res *hal.Response) error {
+	return res.Send("BAR")
 })
 
 func run() int {
-    robot, err := hal.NewRobot()
-    if err != nil {
-        hal.Logger.Error(err)
-        return 1
-    }
+	robot, err := hal.NewRobot()
+	if err != nil {
+		hal.Logger.Error(err)
+		return 1
+	}
 
-    robot.Handle(
-        pingHandler,
-        echoHandler,
-    )
+	tableFlipHandler := &hal.BasicHandler{
+		Method:  hal.HEAR,
+		Pattern: `tableflip`,
+		Run: func(res *hal.Response) error {
+			return res.Send(`(╯°□°）╯︵ ┻━┻`)
+		},
+	}
 
-    if err := robot.Run(); err != nil {
-        hal.Logger.Error(err)
-        return 1
-    }
-    return 0
+	beerHandler := &hal.BasicHandler{
+		Method:  hal.HEAR,
+		Pattern: `beer`,
+		Run: func(res *hal.Response) error {
+			return res.Reply(`delicious...beer`)
+		},
+	}
+
+	robot.Handle(
+		//echoHandler,
+		//handler.Echo,
+		handler.Ping,
+		handler.Uptime,
+		tableFlipHandler,
+		beerHandler,
+		fooHandler,
+	)
+
+	if err := robot.Run(); err != nil {
+		hal.Logger.Error(err)
+		return 1
+	}
+	return 0
 }
 
 func main() {
-    os.Exit(run())
+	os.Exit(run())
 }
